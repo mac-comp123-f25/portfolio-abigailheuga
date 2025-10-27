b8a177f710e6f9677af9eb8ab493cd3b038a4207
abigailheuga <aheuga@macalester.edu>
2025-10-26 20:07:30 -0500

    Update word_freq.py

--- src/ica/13-word_freq/word_freq.py
+++ src/ica/13-word_freq/word_freq.py
@@ -1,6 +1,20 @@
+import string
+
+
 def compute_frequencies(filename):
-    with open(filename, 'r') as f:
-        contents = f.read()
-    print(contents)
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+
+    clean_words = []
+    words = text.split()
+    for word in words:
+        newWord = word.strip(string.punctuation)
+        clean_words.append(newWord)
+    print(clean_words)
+
+    for w in clean_words:
+        print(w)
+
 
 compute_frequencies("../TextFiles/alice.txt")
