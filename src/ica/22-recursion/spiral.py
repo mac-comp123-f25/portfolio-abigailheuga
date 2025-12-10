ae038e14c4e4cf1dad61f8ced5b3a8eb5cf2fa3b
abigailheuga <aheuga@macalester.edu>
2025-12-10 11:56:02 -0600

    Update spiral.py

--- src/ica/22-recursion/spiral.py
+++ src/ica/22-recursion/spiral.py
@@ -2,7 +2,13 @@ import turtle
 
 
 def spiral_in(spiro_turt, side_len):
-    pass
+    if side_len < 5:
+        spiro_turt.right(360*2)     #turtle celebration
+        return
+    else:
+        spiro_turt.forward(side_len)
+        spiro_turt.left(90)
+        spiral_in(spiro_turt, side_len - 5)
 
 
 def check_spiral_in(size: int) -> None:
@@ -14,4 +20,4 @@ def check_spiral_in(size: int) -> None:
 
 
 if __name__ == '__main__':
-    check_spiral_in(20)
+    check_spiral_in(200)
